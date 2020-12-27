22dbf8a8ef9a04879228b417d1e97b2a80b4dd3f
George Cover <5285122+TheOptimist@users.noreply.github.com>
2020-12-27 01:30:37 -0500

    Configuring sudoers permission

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -6,7 +6,17 @@
     systemPackages = with pkgs; [ bash fish zsh ];
     darwinConfig = "$HOME/.nixpkgs/darwin/configuration.nix";
     shells = with pkgs; [ bash fish zsh ];
-  }
+
+    etc = {
+     "sudoers.d/10-nix-commands".text = ''
+       george ALL=(ALL:ALL) NOPASSWD: /run/current-system/sw/bin/darwin-rebuild, \
+                                      /run/current-system/sw/bin/nix*, \
+                                      /run/current-system/sw/bin/ln, \
+                                      /run/store/*/activate, \
+                                      /bin/launchctl
+      '';
+    };
+  };
 
   services.nix-daemon.enable = true;
 
