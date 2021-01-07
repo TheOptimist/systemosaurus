3ff622956ffd84f659560204ed1fd017a40e96f8
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-01-06 21:33:33 -0500

    Moving file around logically

--- home/git.nix
+++ home/git.nix
@@ -1,8 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
-{
+let 
+  inherit (lib) optional flatten;
+  inherit (lib.systems.elaborate { system = builtins.currentSystem; }) isLinux isDarwin;
+
+in {
   programs.git = {
     enable = true;
+
+    userName = "George Cover";
+    userEmail = "5285515+TheOptimist@users.noreply.github.com";
+    
+    ignores = flatten [
+      (optional isDarwin ".DS_Store")
+    ];
+
+    aliases = {
+      l = "log --graph --abbrev-commit --decorate --format=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset) %s %C(italic)- %an%C(reset)%C(magenta bold)%d%C(reset)' --all";
+    };
+    
     package = pkgs.buildEnv {
       name = "customGit";
       paths = with pkgs.gitAndTools; [
@@ -18,15 +34,6 @@
         side-by-side = true;
         syntax-theme = "gruvbox";
       };
-    };
-
-    userName = "George Cover";
-    userEmail = "5285515+TheOptimist@users.noreply.github.com";
-
-    ignores = [ ".DS_Store" ];
-
-    aliases = {
-      l = "log --graph --abbrev-commit --decorate --format=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset) %s %C(italic)- %an%C(reset)%C(magenta bold)%d%C(reset)' --all";
     };
   };
 }    
