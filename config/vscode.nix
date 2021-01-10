f1b4c06a6f8aebb9d61c5b11ff8029c2bc4a8444
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-01-09 19:48:53 -0500

    Adding themes from marketplace into VSCode. More to come.

--- config/vscode.nix
+++ config/vscode.nix
@@ -1,9 +1,22 @@
 { config, pkgs, ... }:
 
+# TODO: Save settings file here, create in store, and link to correct location
 let
   extensions = (with pkgs.vscode-extensions; [
     bbenoist.Nix
-    pkief.material-icon-theme
+  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      name = "Material-theme";
+      publisher = "zhuangtongfa";
+      version = "3.9.12";
+      sha256 = "017h9hxplf2rhmlhn3vag0wypcx6gxi7p9fgllj5jzwrl2wsjl0g";
+    }
+    {
+      name = "vscode-icons";
+      publisher = "vscode-icons-team";
+      version = "11.1.0";
+      sha256 = "1xrz9f0nckx29wxpmlj1dqqiaal3002xwgzz5p9iss119sxgpwrx";
+    }
   ]);
   customVSCode = pkgs.vscode-with-extensions.override {
     vscodeExtensions = extensions;
