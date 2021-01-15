6320142206a7c99117cdfcd714f6bd6dfc4327bd
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-01-15 08:45:14 -0500

    Just rearranging a few things. Tried to use  but it fails on build?

--- config/shell.nix
+++ config/shell.nix
@@ -1,19 +1,21 @@
 { pkgs, lib, ... }:
 
 let
-  inherit (lib) mkMerge mkForce optionalAttrs;
+  inherit (lib) mkMerge optionalAttrs;
   inherit (lib.systems.elaborate { system = builtins.currentSystem; }) isLinux isDarwin;
 
 in 
 
   mkMerge [
     {
+      # overkill and I'm not using them, but I want to try fish at some point
       environment = {
         systemPackages = with pkgs; [ bash fish zsh ];
       };
 
       programs.zsh.enable = true;
       programs.bash.enable = true;
+      programs.fish.enable = true;
     }
 
     (optionalAttrs isDarwin {
