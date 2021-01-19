e16bf5388e5d754d27eb4eb129765d47d81530f7
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-01-19 15:24:17 -0500

    Removing some cruft, and shifting configuration around

--- config/docker.nix
+++ config/docker.nix
@@ -1,34 +1,37 @@
 { pkgs, lib, ... }:
 
+with lib;
+
 let
-  inherit (lib) mkMerge mkIf optional;
   inherit (lib.systems.elaborate { system = builtins.currentSystem; }) isLinux isDarwin;
 
   homeDirectory = builtins.getEnv "HOME";
   xdgConfigHome = "${homeDirectory}/.config";
   
-in 
+in
   mkMerge [
     # This is guess right now. Haven't attempted a linux install yet.
-    (mkIf isLinux {
+    (optionalAttrs isLinux {
       environment = {
         systemPackages = with pkgs; [
           docker
           docker-machine
           docker-compose
         ];
-
-        variables = {
-          DOCKER_CONFIG = "${xdgConfigHome}/docker";
-          MACHINE_STORAGE_PATH = "${xdgConfigHome}/docker/machine";
-        };
       };
     })
+
+    {
+      environment.variables = {
+        DOCKER_CONFIG = "${xdgConfigHome}/docker";
+        MACHINE_STORAGE_PATH = "${xdgConfigHome}/docker/machine";
+      };
+    }
     
     # TODO: Move away from Homebrew Docker to unify linux and darwin machine configurations
     # Homebrew has all the same packages, which theoretically means I could use the same pattern
     # Linux mechanic above not tested yet either, so no doubt change is a-coming
-    (mkIf isDarwin {
+    (optionalAttrs isDarwin {
       homebrew.casks = [ "docker" ];
     })
     # TODO: Launch Docker on startup?
