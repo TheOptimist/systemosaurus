7224c3170d43a391500b00fe069157fe16a6294e
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-01-28 08:10:24 -0500

    Reorganizing cloud tooling

--- home/cloud/aws.nix
+++ home/cloud/aws.nix
@@ -10,8 +10,6 @@ in {
     shellAliases = {
       # AWS in docker
       aws = "docker run --rm -it -v ${awsConfigHome}:/root/.aws amazon/aws-cli:2.1.18";
-      # Terraform in docker
-      terraform = "docker run --rm -it -v \${pwd}:/workspace -w /workspace hashicorp/terraform:0.14.4";
     };
 
     initExtra = ''
--- home/cloud/default.nix
+++ home/cloud/default.nix
@@ -3,4 +3,11 @@
     ./aws.nix
     ./oci
   ];
+
+  programs.zsh = {
+    shellAliases = {
+      # Terraform in docker
+      terraform = "docker run --rm -it -v \$(pwd):/workspace -w /workspace hashicorp/terraform:0.14.4";
+    };
+  };
 }
