340981a0b486862c93db4456478311491d6e526d
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-02-23 07:24:22 -0500

    Creating binary to run OCI in docker container for portability with scripts

--- home/cloud/oci/default.nix
+++ home/cloud/oci/default.nix
@@ -1,19 +1,19 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 
 let
-  ociConfigPath = "${config.xdg.configHome}/oci";
-  ociConfigFile = "${ociConfigPath}/config";
+  ociConfigPath     = "${config.xdg.configHome}/oci";
+  oci = pkgs.writeShellScriptBin "oci" ''
+    docker run --rm -it -v ${ociConfigPath}:/root/.oci jpoon/oci-cli $@
+  '';
+
   #functions = builtins.readFile ./functions.sh;
 
 in {
+  home.packages = [ oci ];
+
   programs.zsh = {
-
-    shellAliases = {
-      oci = "docker run --rm -it -v ${ociConfigPath}:/root/.oci jpoon/oci-cli";
-    };
-
     sessionVariables = {
-      OCI_TENANT="$( cat ${ociConfigFile} | grep tenancy | cut -d'=' -f2 )";
+      OCI_TENANT="$( cat ${ociConfigPath}/config | grep tenancy | cut -d'=' -f2 )";
     };
 
     initExtra = ''
