f315d75c55a434857eb0fb3685fa3097c65a481d
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-02-24 10:34:19 -0500

    Add Terraform binary, albeit specific for OCI right now

--- home/cloud/default.nix
+++ home/cloud/default.nix
@@ -1,13 +1,16 @@
-{
+{ pkgs, config, ... }:
+
+let
+  # Yeah, this should probably be OCI specific really...
+  terraform = pkgs.writeShellScriptBin "terraform" ''
+    "docker run --rm -it -v ${config.xdg.configHome}/oci:/keys:ro -v \$(pwd):/workspace -w /workspace hashicorp/terraform:0.14.4 $@"
+  '';
+
+in {
   imports = [
     ./aws.nix
     ./oci
   ];
 
-  programs.zsh = {
-    shellAliases = {
-      # Terraform in docker
-      terraform = "docker run --rm -it -v /Users/george/.config/oci:/keys:ro -v \$(pwd):/workspace -w /workspace hashicorp/terraform:0.14.4";
-    };
-  };
+  home.packages = [ terraform ];
 }
