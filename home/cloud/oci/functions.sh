1a122cb9b298689ee032f08d247d87782f1ed477
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-03-17 14:06:53 -0400

    Change OCI functions

--- home/cloud/oci/functions.sh
+++ home/cloud/oci/functions.sh
@@ -1,26 +1,20 @@
 function compartment_id() {
-  oci iam compartment list --compartment-id-in-subtree true \
-  | jq -r --arg value "$1" '.data[] | select(.name==$value) | .id'
+  local all_compartments="$( oci iam compartment list --compartment-id-in-subtree true --all )"
+  echo "${all_compartments}" | jq -r --arg value "$1" '.data[] | select(.name==$value) | .id'
 }
 
 function my_compartment_id() {
-  if [[ -z ${MY_COMPARTMENT_ID+x} ]]; then
-    MY_COMPARTMENT_ID="$(compartment_id 'cover')"
-  fi
-  echo "${MY_COMPARTMENT_ID}"
+  compartment_id 'cover'
 }
 
 function customer_stack() {
-  if [[ -z ${CUSTOMER_STACK} ]] || [[ "$2" == "--force" ]]; then
-    CUSTOMER_STACK="$(oci resource-manager stack list --compartment-id "$(my_compartment_id)" --display-name "$1")"
-  fi
-  echo "${CUSTOMER_STACK}"
+  echo "$( oci resource-manager stack list --compartment-id "$(my_compartment_id)" --display-name "$1" )"
 }
 
 function most_recent_job() {
-  oci resource-manager job list --stack-id "$(customer_stack | jq -r '.data[].id')" | jq -r '.data | sort_by(."time-created") | .[-1]'
+  oci resource-manager job list --stack-id "$(customer_stack "$1" | jq -r '.data[].id')" | jq -r '.data | sort_by(."time-created") | .[-1]'
 }
 
 function job_logs() {
-  echo -e "$(oci resource-manager job get-job-logs-content --job-id "$(most_recent_job | jq -r '.id')" | jq -r '.data')"
+  echo -e "$(oci resource-manager job get-job-logs-content --job-id "$(most_recent_job "$1" | jq -r '.id')" | jq -r '.data')"
 }
