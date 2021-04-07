aada605386ecbbc2e94aa7d2b2158242e2f98a93
George Cover <5285515+TheOptimist@users.noreply.github.com>
2021-04-07 07:08:24 -0400

    Function for getting stack outputs from OCI

--- home/cloud/oci/functions.sh
+++ home/cloud/oci/functions.sh
@@ -11,12 +11,12 @@ function my_compartment_id() {
   compartment_id 'cover'
 }
 
-function customer_stack() {
-  echo "$( oci resource-manager stack list --compartment-id "$(my_compartment_id)" --display-name "$1" )"
+function get_stack() {
+  oci resource-manager stack list --compartment-id "$(my_compartment_id)" | jq --arg s "$1" '.data[] | select(."display-name" | startswith($s))'
 }
 
 function most_recent_job() {
-  oci resource-manager job list --stack-id "$(customer_stack "$1" | jq -r '.data[].id')" | jq -r '.data | sort_by(."time-created") | .[-1]'
+  oci resource-manager job list --stack-id "$(get_stack "$1" | jq -r '.data[].id')" | jq -r '.data | sort_by(."time-created") | .[-1]'
 }
 
 function job_logs() {
@@ -34,4 +34,15 @@ function get_public_ip() {
   local -r instance_id="$(get_instance $compartment_id $2 | jq -r '.id')"
   local -r vnic_id="$(oci compute vnic-attachment list --compartment-id $compartment_id | jq -r --arg v "$instance_id" '.data[] | select(."instance-id"==$v) | ."vnic-id"')"
   oci network vnic get --vnic-id $vnic_id | jq -r '.data."public-ip"'
+}
+
+function get_stack_outputs {
+  local -r stack_id="$( get_stack $1 | jq -r '.id' )"
+  local -r jobs="$( oci resource-manager job list --compartment-id $(my_compartment_id) --stack-id ${stack_id} --sort-by TIMECREATED --sort-order DESC )"
+  local -r latest_job_id="$( echo "$jobs" | jq '.data[] | select (. | .operation == "APPLY")' | jq -rs '.[0].id' )"
+  local -r state_file=$(mktemp)
+  oci resource-manager job get-job-tf-state --job-id ${latest_job_id} --file "${state_file}"
+  local -r readonly outputs="$(cat "$state_file" | jq .outputs)"
+  rm -f "$state_file"
+  echo $outputs | tr -d '\n\r\t'
 }
