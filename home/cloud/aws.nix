26d7cdc35869088fee70a9631c49d5281eb22251
George Cover <5285122+TheOptimist@users.noreply.github.com>
2021-07-22 10:58:33 -0400

    Use latest AWS docker container

--- home/cloud/aws.nix
+++ home/cloud/aws.nix
@@ -9,7 +9,7 @@ in {
 
     shellAliases = {
       # AWS in docker
-      aws = "docker run --rm -it -v ${awsConfigHome}:/root/.aws amazon/aws-cli:2.1.18";
+      aws = "docker run --rm -it -v ${awsConfigHome}:/root/.aws amazon/aws-cli:latest";
     };
 
     initExtra = ''
